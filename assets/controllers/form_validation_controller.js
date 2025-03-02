438f592a4159d4aadc19bd2f16f2c80ccfdea12a
TaniaGA01 <tanygaitan@gmail.com>
2025-03-02 20:15:53 +0100

    validation date field

--- assets/controllers/form_validation_controller.js
+++ assets/controllers/form_validation_controller.js
@@ -2,7 +2,7 @@ import { Controller } from '@hotwired/stimulus';
 import { alertHandle } from './utils/alertHandler'
 
 export default class extends Controller {
-    static targets = ['input', 'questions', 'submit'];
+    static targets = ['input', 'questions', 'submit', 'form'];
 
     connect() {
         this.submitTargets[0].onclick = (event) => this.validateFieldForm(event);
@@ -30,14 +30,16 @@ export default class extends Controller {
                         alertHandle();
                     }
                 }else{
-                    if(emailRegex.inputText.test(input.value) === false){
-                        input.classList.remove('focus:outline-red-500','border-1','border-red-500');
-                        input.classList.add('focus:outline-red-500','border-1','border-red-500');
-                        alertHandle();
-                    }else{
-                        input.classList.remove('focus:outline-red-500','border-1','border-red-500');
-                        input.classList.add('focus:outline-green-500','border-1','border-green-500');
-                        alertHandle();
+                    if(input.type !== "date"){
+                        if(emailRegex.inputText.test(input.value) === false){
+                            input.classList.remove('focus:outline-red-500','border-1','border-red-500');
+                            input.classList.add('focus:outline-red-500','border-1','border-red-500');
+                            alertHandle();
+                        }else{
+                            input.classList.remove('focus:outline-red-500','border-1','border-red-500');
+                            input.classList.add('focus:outline-green-500','border-1','border-green-500');
+                            alertHandle();
+                        }
                     }
                 }
             })
@@ -45,16 +47,31 @@ export default class extends Controller {
         }
     }
 
+    validateDateField(){
+        if(this.inputTargets.length){
+            [...this.inputTargets].filter(input => input.type === 'date').forEach(input => {
+                if(input.valueAsDate !== null){
+                    input.classList.remove('focus:outline-red-500','border-1','border-red-500');
+                    input.classList.add('focus:outline-green-500','border-1','border-green-500');
+                }else{
+                    input.classList.add('focus:outline-red-500','border-1','border-red-500');
+                    input.classList.remove('focus:outline-green-500','border-1','border-green-500');
+                }
+            })}
+    }
+
     validateFieldForm(event){
         if(this.inputTargets.length){
             [...this.inputTargets].filter(input => input).forEach(input => { 
                 
-                if(input.value !== ''){
+                if(input.value !== '' && input.valueAsDate !== null){
                     input.classList.remove('focus:outline-red-500','border-1','border-red-500');
                     alertHandle();
                     if(this.questionsTargets){
                         [...this.questionsTargets].forEach(item => item.remove());
                     }
+                    input.classList.add('focus:outline-green-500','border-1','border-green-500');
+                    this.formTargets[0].submit()
                 }else{
                     alertHandle();
                     input.classList.add('focus:outline-red-500','border-1','border-red-500');
--- src/Form/PollType.php
+++ src/Form/PollType.php
@@ -53,8 +53,9 @@ class PollType extends AbstractType
                 'attr' => [
                     'placeholder' => "Date d'expiration",
                     'class' => 'form-control block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white  -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline  focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6',
-                    // 'data-form-validation-target' => 'input',
-                    // 'data-action'=>'keydown->form-validation#validateField'
+                    'data-form-validation-target' => 'input',
+                    'data-action'=>'keydown->form-validation#validateField',
+                    'data-action'=>'input->form-validation#validateDateField'
                 ]
             ])
             ->add('questions', CollectionType::class, [
